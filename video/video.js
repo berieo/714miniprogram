394326196d136d82c00d68211234825f817e21d4
Andthen <34112784+berieo@users.noreply.github.com>
2018-03-29 18:26:46 +0800

    Add files via upload

---
+++ video/video.js
@@ -0,0 +1,52 @@
+Page({
+  data: {
+    text: "This is page data.",
+    imageUrl: "../resource/images/photo1.jpg"
+  }, 
+  jumpTo1: function(){
+    wx.navigateTo({
+      url: '../video/jumpTo1/jumpTo1'
+    })
+
+  },
+  jumpTo2: function () {
+    wx.navigateTo({
+      url: '../video/jumpTo2/jumpTo2'
+    })
+  },
+  jumpTo3: function () {
+    wx.navigateTo({
+      url: '../video/jumpTo3/jumpTo3'
+    })
+  },
+  jumpTo4: function () {
+    wx.navigateTo({
+      url: '../video/jumpTo4/jumpTo4'
+    })
+  },
+  jumpTo5: function () {
+    wx.navigateTo({
+      url: '../video/jumpTo5/jumpTo5'
+    })
+  },
+  jumpTo6: function () {
+    wx.navigateTo({
+      url: '../video/jumpTo6/jumpTo6'
+    })
+  },
+  jumpTo7: function () {
+    wx.navigateTo({
+      url: '../video/jumpTo7/jumpTo7'
+    })
+  },
+  jumpTo8: function () {
+    wx.navigateTo({
+      url: '../video/jumpTo8/jumpTo8'
+    })
+  }
+
+  })
+
+
+
+
